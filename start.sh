6d5709213361f635149caeedddfb4b38ffc204e0
Austin West <austin.w.west@stonybrook.edu>
2024-04-20 14:01:12 -0400

    more script

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install npm
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
 npm run build
 node server.js secretSession
