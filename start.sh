0e36b23dc2f45fa2af42ea37a00ebe0ed1402c08
Austin West <austin.w.west@stonybrook.edu>
2024-05-01 20:30:50 -0400

    Change start script

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 sudo apt-get update
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install -y nodejs
-npm run build
+npm install node-fetch@2
+npm i -y
 node server.js secretSession
